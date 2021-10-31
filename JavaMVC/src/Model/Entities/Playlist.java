a76114bc7a05ee0bd9f7dbfa5b316dc49f5f5838
alc9026 <alc9026@rit.edu>
2021-10-31 14:54:27 -0400

    fixed dash delimeters and runtime conversion in playlist tostring

--- JavaMVC/src/Model/Entities/Playlist.java
+++ JavaMVC/src/Model/Entities/Playlist.java
@@ -89,7 +89,7 @@ public class Playlist {
      */
     @Override
     public String toString() {
-        return String.format("Playlist: %s -- Number of songs %s -- Runtime %s%n", this.name, getSongs().size(), this.runtime);
+        return String.format("Playlist: %s -- Number of songs %s -- Runtime %s%n", this.name, getSongs().size(), this.runtime / 1000 / 60);
     }
 
     public String getPlaylistID() {
