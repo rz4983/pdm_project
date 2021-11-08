ae1aa633ceb83659cf2831fbe26acdf12dd84e0f
Srikar Sundaram <ss8931@rit.edu>
2021-11-07 23:02:51 -0500

    add email for song plays

--- JavaMVC/src/Model/Entities/Song.java
+++ JavaMVC/src/Model/Entities/Song.java
@@ -1,5 +1,6 @@
 package Model.Entities;
 
+import Controller.Application;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,14 +15,6 @@ import java.sql.Statement;
  * @version 2021.10.24.1
  */
 public class Song {
-    /** The unique alphanumeric string that identifies this song. */
-    private final String songID;
-    /** The length of the song in ms. */
-    private final int length;
-    /** The name of the song. */
-    private final String title;
-    /** The date the song was released in format yyyy-mm-dd (or yyyy if full date is unavailable). */
-    private final String songReleaseDate;
 
     private static Statement stmt;
 
@@ -33,11 +26,28 @@ public class Song {
     }
 
     /**
+     * The unique alphanumeric string that identifies this song.
+     */
+    private final String songID;
+    /**
+     * The length of the song in ms.
+     */
+    private final int length;
+    /**
+     * The name of the song.
+     */
+    private final String title;
+    /**
+     * The date the song was released in format yyyy-mm-dd (or yyyy if full date is unavailable).
+     */
+    private final String songReleaseDate;
+
+    /**
      * Constructor is called after needed data from DB is queried.
      *
-     * @param songID unique string identifier
-     * @param length length of song in ms
-     * @param title name of the song
+     * @param songID          unique string identifier
+     * @param length          length of song in ms
+     * @param title           name of the song
      * @param songReleaseDate the date in which the song was released
      */
     public Song(String songID, int length, String title, String songReleaseDate) {
@@ -48,7 +58,7 @@ public class Song {
     }
 
     public String queryArtist() throws SQLException {
-        String query = "Select get_song_artist('" + this.songID +"');";
+        String query = "Select get_song_artist('" + this.songID + "');";
 
         ResultSet rs = stmt.executeQuery(query);
 
@@ -56,7 +66,7 @@ public class Song {
         while (rs.next()) {
             songArtist = rs.getString("get_song_artist");
         }
-        if (songArtist == null || songArtist.equals("")){
+        if (songArtist == null || songArtist.equals("")) {
             songArtist = "N/A";
         }
 
@@ -69,27 +79,29 @@ public class Song {
 
     public int queryNumPlayed() throws SQLException {
         int numPlays = 0;
-        String getCount = "SELECT COUNT(*) FROM \"Plays\" WHERE \"songID\" = '" + this.songID + "';";
+        String getCount =
+            "SELECT COUNT(*) FROM \"Plays\" WHERE \"songID\" = '" + this.songID + "' and email = '"
+                + Application.getCurrentUser().getEmail() + "';";
 
         ResultSet rs = stmt.executeQuery(getCount);
 
-        while(rs.next()){
-             numPlays = rs.getInt("count");
+        while (rs.next()) {
+            numPlays = rs.getInt("count");
         }
         return numPlays;
     }
 
-    public String queryAlbum() throws SQLException{
-        String query = "Select get_song_album('" + this.songID +"');";
+    public String queryAlbum() throws SQLException {
+        String query = "Select get_song_album('" + this.songID + "');";
 
         ResultSet rs = stmt.executeQuery(query);
 
         String songAlbum = "";
 
-        while (rs.next()){
+        while (rs.next()) {
             songAlbum = rs.getString("get_song_album");
         }
-        if (songAlbum == null || songAlbum.equals("")){
+        if (songAlbum == null || songAlbum.equals("")) {
             songAlbum = "N/A";
         }
 
@@ -100,13 +112,15 @@ public class Song {
      * @return TODO
      */
     @Override
-    public String toString()  {
+    public String toString() {
 
         try {
-            return String.format("Song: %s -- Artist: %s -- Album: %s -- Minutes: %.2f -- Release Date: %s -- " +
-                            "Number of plays: %d", title, queryArtist(), queryAlbum(),
-                    length / 1000.0 / 60, songReleaseDate, queryNumPlayed());
-        } catch (SQLException ignore) {}
+            return String.format(
+                "Song: %s -- Artist: %s -- Album: %s -- Minutes: %.2f -- Release Date: %s -- " +
+                    "Number of plays: %d", title, queryArtist(), queryAlbum(),
+                length / 1000.0 / 60, songReleaseDate, queryNumPlayed());
+        } catch (SQLException ignore) {
+        }
         return "SQL Exception";
     }
 }
