c2e720e5132db9fa21ce7460b09bedf9ab51afb2
alc9026 <alc9026@rit.edu>
2021-11-20 18:09:41 -0500

    I'm dumb, spelled recommendation correctly this time

--- JavaMVC/src/Model/QueryDB/Recommendation.java
+++ JavaMVC/src/Model/QueryDB/Recommendation.java
@@ -5,6 +5,6 @@ package Model.QueryDB;
  * other 5 commands will be here as they do not modify the database, they are only display selects
  *
  */
-public class Reccomendations {
+public class Recommendation {
 
 }
